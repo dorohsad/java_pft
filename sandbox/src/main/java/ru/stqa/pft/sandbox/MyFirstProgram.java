5cfc4f1aaed7517358a7882db434639664f4f302
dorohsad <dorohsad@mail.ru>
2016-10-08 16:51:20 +0700

    Пробные задания: прямоугольник

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,8 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
 	public static void main(String[] args){
-
+	Square s = new Square(5.0);
+		System.out.println("Площадь прямоугольника со стороной " + s.x + " см равна "+ s.area());
 System.out.println("Hellow, world!");
 	}
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Doroh on 08.10.2016.
+ */
+public class Square {
+    double x;
+    public Square (double x){
+        this.x=x;
+    }
+    public double area (){
+        return this.x * this.x ;
+    }
+
+}
