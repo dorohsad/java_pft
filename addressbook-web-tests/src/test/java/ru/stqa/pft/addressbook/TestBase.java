2bd6f50b0c548411bd531c920cc5ccf68e62d757
dorohsad <dorohsad@mail.ru>
2016-10-15 15:36:17 +0700

    Добавлен класс для удалениня групп

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -0,0 +1,85 @@
+package ru.stqa.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Doroh on 15.10.2016.
+ */
+public class TestBase {
+    FirefoxDriver wd;
+
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost:8090/addressbook/");
+
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+
+    protected void returnToGroupePage() {
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    protected void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GrouData grouData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(grouData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(grouData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(grouData.getFooter());
+    }
+
+    protected void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    protected void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+
+    protected void deleteGroups() {
+        wd.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
+    }
+}
